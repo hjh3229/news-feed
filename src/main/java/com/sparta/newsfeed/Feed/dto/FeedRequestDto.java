f98953b0dcef53ba3a7cb8394307b6333258e38c
Han JiHun <hjh_3229@naver.com>
2023-07-03 10:16:54 +0900

    requestdto에서 제목과 설명은 빈칸이 가능하도록

--- src/main/java/com/sparta/newsfeed/Feed/dto/FeedRequestDto.java
+++ src/main/java/com/sparta/newsfeed/Feed/dto/FeedRequestDto.java
@@ -5,10 +5,8 @@ import lombok.Getter;
 
 @Getter
 public class FeedRequestDto {
-    @NotBlank
     private String title;
     @NotBlank
     private String url;
-    @NotBlank
     private String contents;
 }
--- src/main/java/com/sparta/newsfeed/User/controller/UserController.java
+++ src/main/java/com/sparta/newsfeed/User/controller/UserController.java
@@ -5,8 +5,6 @@ import com.sparta.newsfeed.User.dto.SignupRequestDto;
 import com.sparta.newsfeed.User.dto.SignupResponseDto;
 import com.sparta.newsfeed.User.service.UserService;
 import lombok.RequiredArgsConstructor;
-amework.web.bind.annotation.RestController;
-=======
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,10 +14,8 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/sign-up")
-    public SignupResponseDto createUser(@RequestBody SignupRequestDto requestDto){
-        return userService.createUser(requestDto);
-    }
-
-
+//    @PostMapping("/sign-up")
+//    public SignupResponseDto createUser(@RequestBody SignupRequestDto requestDto){
+//        return userService.createUser(requestDto);
+//    }
 }
